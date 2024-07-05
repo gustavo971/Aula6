6d3bfa9f24827d8dd31d1c0833f1f272f95d7bc5
Anderson Gomes <150376296+Gomezdev5@users.noreply.github.com>
2024-07-05 19:56:37 +0000

    a1

--- qt05.py
+++ qt05.py
@@ -1,5 +1,30 @@
 '''Exercício 5: Escreva um programa que peça ao usuário para digitar uma frase e, em seguida,
  conte quantas vogais (a, e, i, o, u) existem na frase utilizando um loop "for".'''
 
+frase = input("Digite uma frase")
+total_a, total_e, total_i, total_o,total_u = 0,0,0,0,0
+
+for letra in frase:
+    
+
+    if letra == "a":
+        total_a += 1
+    elif letra == "e":
+        total_e += 1
+    elif letra == "i":
+        total_i += 1
+    elif letra == "o" :
+        total_o += 1
+    elif letra == "u":
+        total_u += 1
+
+
+print("O total de vogais é a:", total_a)
+print("O total de vogais é e:", total_e)
+print("O total de vogais é i:", total_i)
+print("O total de vogais é o:", total_o)
+print("O total de vogais é u:", total_u)
+
+
 
 
